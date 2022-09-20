1adf9f660826eb32866855a9a4525e859333d1b5
gabrielderome <111153751+gabrielderome@users.noreply.github.com>
2022-09-19 23:07:35 -0400

    Add files via upload

---
+++ c_code/tp1numeroA.c
@@ -0,0 +1,43 @@
+#include<stdio.h>//entete  etudiant:Gabriel Derome
+int main()//principal
+{
+    while (1)//boucle
+    {
+        float weight;//definition de variable
+        float hight;//definition de variable
+        printf("\nveuillez entrer le poids du patient(kg), suivis de ca grandeur(m): \n");//prompt
+        scanf("%f%f", & weight, & hight);//reponses
+        float imc = weight/(hight*hight);//calcul de l'imc
+        printf("\nvotre indice de masse corporelle est de:%f\n", imc);//affichage de l'imc
+        printf("le poids du patient en kg est de: %.2f", weight);//affichage du poids
+        printf("la taille du patient en m: %.2f", hight);//affichage de la taille
+        if (imc < 18.5)//if statement
+        {
+            printf("\nMAIGREUR, RISQUE ELEVE A ACCRU");//avertissement
+        }
+        else if (imc < 25)//else if statement
+        {
+        printf("\nPOIDS NORMAL, RISQUE FAIBLE");//avertissement
+        }
+        else if (imc < 30)//else if statement
+        {
+        printf("\nEMBONPOINT, RISQUE ELEVE");//avertissement
+        }
+        else//else statement
+        {
+        printf("\nOBESITE, RISQUE TRES ELEVE");//avertissement
+        }
+        int compte_de_patient;//compte de client a l'origine
+        compte_de_patient = compte_de_patient + 1;//compte d'iteration
+        printf("\nClient#:%d",compte_de_patient);//affichage du compte de client
+        int choice;//definition de variable
+        printf("\nAvez vous fini? 2 pour oui, 1 pour Non: ");//l'utilisateur veut-il continuer?
+        scanf("%d", &choice);//reponse
+        if (choice == 2)//if statement
+         {
+           printf("\ncompte total de client:%d\n",compte_de_patient);//affichage du nombre de patients
+           break;//la boucle est boucle
+         }
+        }
+    return 0;
+}
---
+++ c_code/tp1numeroB.c
@@ -0,0 +1,73 @@
+#include <stdarg.h>//entete etudiant:Gabriel Derome
+#include <stdio.h>
+#include <stdlib.h>
+int main() {//principale
+  char shape;//definition de variables
+  char circle = 'c';
+  char rectangle = 'r';
+  char square = 'k';
+  int choice;
+  float maxperimetrec = 0.00;
+  float maxairer = 0.00;
+  float mincote = 0;
+  int reccompte = 0;
+  float sumperimetre = 0;
+  while(1)//boucle
+  {
+    printf("tapez c pour cercle \ntapez r pour rectangle \ntapez k pour carre\n");//choix de la forme
+    scanf("\n %c", &shape);
+    if (shape == circle) {//CERCLE
+      float rayon;//definition de variable
+      printf("veuillez entrer le rayon du cercle: \n");//entre du rayon
+      scanf("\n %f", &rayon);
+      float airec = 3.14159 * rayon * rayon;//calculs
+      float perimetrec = 3.14159 * 2 * rayon;
+      if (perimetrec > maxperimetrec){//max perimetre?
+         maxperimetrec = perimetrec;
+      }
+      printf("l'aire du cercle = %.2f\n", airec);//affichage des donnes
+      printf("le perimetre du cercle = %.2f", perimetrec);                                                                 
+    } else if (shape == rectangle) {//RECTANGLE
+      float base;//definition de variables
+      float hauteur;
+      printf("veuillez entrer la hauteur puis, la base du rectangle\n");//entre de la base et de la hauteur
+      scanf("\n %f", &base);
+      scanf("\n %f", &hauteur);
+      float airer = base * hauteur;//calculs
+      float perimetrer = 2 * base + 2 * hauteur;
+      printf("l'aire du rectangle= %2.f\n", airer);//affichage des donnes
+      printf("le perimetre du rectangle = %2.f\n", perimetrer);                                                      
+      reccompte = reccompte + 1;//compte de rectangles
+      sumperimetre = sumperimetre + perimetrer;//sum des perimetres
+      if (airer > maxairer){//max aire?
+         maxairer = airer;
+      }
+    } else if (shape == square){//CARRE
+      float cote;//definition de variable
+      printf("veuillez entrer le cote du carre\n");//entre du cote
+      scanf("\n %f", & cote);
+      float airek = cote * cote;//calculs
+      float perimetrek = cote * 4;
+      printf("l'aire du carre= %2.f\n", airek);//affichage des donnes
+      printf("le perimetre du carre= %2.f\n", perimetrek);
+      if ((cote < mincote) || (mincote == 0)){//min cote?
+         mincote = cote;
+      }
+    } else {//not a valid shape
+      printf("entree invalide");
+    }
+    printf("\nAvez vous fini? 2 pour oui, 1 pour Non: ");//l'utilisateur veut-il continuer?          
+    scanf("\n %d", &choice);
+    if (choice == 2)                                            
+    {
+    break;//la boucle est boucle                                               
+    }
+  }
+  float avgperi = sumperimetre / reccompte;//moyenne des perimetres
+  printf("nombre de rectangle construit: %d\n", reccompte);//affichage d'info
+  printf("le cercle avec le plus grand perimetre as un perimetre de: %.6f\n", maxperimetrec);
+  printf("le rectangle avec la plus grande aire as une aire de: %.6f\n", maxairer);
+  printf("le carre avec le plus petit cote avait un cote de: %.6f\n", mincote);
+  printf("le perimetre moyen des rectangles: %2.f\n", avgperi);
+  return 0;
+}

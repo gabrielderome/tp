a96932c5ff35f50fee07a2afe1ef0fadc971e89b
gabrielderome <gabriel.derome@gmail.com>
2022-12-10 23:56:34 -0500

    fixed main

--- TP3/tp3C/Testpersonne.java
+++ TP3/tp3C/Testpersonne.java
@@ -1,18 +1,21 @@
-class testpersonne {
+//Gabriel Derome 20216134
+class Testpersonne {
     public static void main(String[] args) {
-        //create 5 persons and and add them to a table with an incrementing id
+        //creer 5 nouvelles personnes et les ajouter dans un tableau
         personne[] pers = new personne[5];
         pers[0] = new personne("16/05/2001", 2);
         pers[1] = new personne("02/05/1990");
         pers[2] = new personne("23/11/1996", 5);
         pers[3] = new personne("19/02/2000", 0);
         pers[4] = new personne("30/05/1991", 2);
+        //afficher la legende de la table
         System.out.println("\n\nindex | naissance |nbcafe");
+        //afficher les donnees de la table
         for (int i = 0; i < 5; i++) {
             System.out.println((i + 1)+ "     |" + pers[i].getNaissance() + " |" + pers[i].getNbcafe());
         }
-        System.out.println("\n\n");
-        //print the table
+        System.out.println("\n");
+        //afficher les donnees des personnes avec la methode afficher() (test de la methode)
         for (int i = 0; i < 5; i++) {
             if (i == 0){
                 System.out.println(pers[i].afficher("Information de la premiere personne:"));
@@ -24,8 +27,8 @@ class testpersonne {
                 System.out.println(pers[i].afficher("Information de la " + (i + 1) + "eme personne:"));
             }
         }
-        System.out.println("\n\n");
-        //determine who drinks the most coffee and print "la personne qui boit le plus de cafe est la personne #{id}"
+        System.out.println("\n");
+        //determiner et afficher la personne qui boit le plus de cafe
         int max = 0;
         int id = 0;
         for (int i = 0; i < 5; i++) {
@@ -35,37 +38,36 @@ class testpersonne {
             }
         }
         System.out.println("la personne qui boit le plus de cafe est la personne #" + (id + 1) + "\n\n");
-        //substract 1 from everyone who has at least 1 coffee
-       for (int x = 0; x < 5; x++) {
+        //soustraire 1 au nombre de cafe de chaque personne
+        for (int x = 0; x < 5; x++) {
            if (pers[x].getNbcafe() > 0) {
                 pers[x].setNbcafe(pers[x].getNbcafe() - 1);
            }
-       }
-       System.out.println("\n\nindex | naissance |nbcafe");
+        }
+        //afficher la legende de la nouvelle table
+        System.out.println("\n\nindex | naissance |nbcafe");
+        //afficher les donnees de la nouvelle table
         for (int i = 0; i < 5; i++) {
             System.out.println((i + 1)+ "     |" + pers[i].getNaissance() + " |" + pers[i].getNbcafe());
         }
-        System.out.println("\n\n");
-        //count the number of people with naissence matching month 05
+        System.out.println("\n");
+        //compte du nombres de personnes avec une date de naissence en mai.
         int count = 0;
         for (int i = 0; i < 5; i++) {
             if (pers[i].getNaissance().substring(3, 5).equals("05")) {
                 count++;
             }
         }
-        System.out.println("il y a " + count + " personnes qui sont nee en mai\n\n");
+        System.out.println("il y a " + count + " personnes qui sont nee en mai\n");
     }
 }
-
 //RESULTATS:
-
 // index | naissance |nbcafe
 // 1     |16/05/2001 |2
 // 2     |02/05/1990 |1
 // 3     |23/11/1996 |5
 // 4     |19/02/2000 |0
 // 5     |30/05/1991 |2
-
 
 
 // Information de la premiere personne::
@@ -90,7 +92,6 @@ class testpersonne {
 
 
 
-
 // la personne qui boit le plus de cafe est la personne #3
 
 
@@ -104,5 +105,6 @@ class testpersonne {
 // 5     |30/05/1991 |1
 
 
-
 // il y a 3 personnes qui sont nee en mai
+
+// gabrielderome@Gabriels-MBP TP3C % 
--- TP3/tp3C/personne.java
+++ TP3/tp3C/personne.java
@@ -1,7 +1,8 @@
-public class personne {
+//Gabriel Derome 20216134
+class personne {
     private String naissance;
     private int nbcafe = 1;
-    //builder. personne() has to be given AT LEAST a birth date, the number of coffees is optional(1 by default)
+    //constructeur pour classe personne
     public personne(String naissance) {
         this.naissance = naissance;
         this.nbcafe = 1;
@@ -10,29 +11,30 @@ public class personne {
         this.naissance = naissance;
         this.nbcafe = nbcafe;
     }
-    //setter setNbcafe() takes an int as it's argument and changes the number of coffee for that person
+    //methode pour editer le nombre de cafe
     public void setNbcafe(int nbcafe) {
         this.nbcafe = nbcafe;
     }
+    //methode pour obtenir la date de naissance et le nombre de cafe
     public String getNaissance() {
         return naissance;
     }
     public int getNbcafe() {
         return nbcafe;
     }
-    //getter afficher() takes a strings as its argument then prints that string with the person's information
+    //la methode afficher() prend une chaine de caracteres en argument et affiche cette chaine avec les informations de la personne
     public String afficher(String str) {
         if (nbcafe < 1) {
             nbcafe = 1;
         }
         return str + ":\n naissance:" + naissance + "\n nbcafe:" + nbcafe + "\n";
     }
-    //main function (printer and adder)
     public static void main(String[] args) {
+        //ajouter pers1, pers2 et pers3
         personne pers1 = new personne("19/05/1997", 4);
         personne pers2 = new personne("31/12/1990");
         personne pers3 = new personne("08/05/1994", 2);
-        //print
+        //afficher leurs informations
         System.out.println(pers1.afficher("Informations de la premiere personne"));
         System.out.println(pers2.afficher("Informations de la deuxieme personne"));
         System.out.println(pers3.afficher("Informations de la troisieme personne"));

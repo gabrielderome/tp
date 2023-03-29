b5e4747f94bf9d7fcdba274c9f35007471c04df3
gabrielderome <gabriel.derome@gmail.com>
2023-03-29 15:03:07 -0400

    add tp2, add data file, import libs and data.

---
+++ econometrie/TP2/tp2.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+
+df = pd.read_stata('/Users/gabrielderome/Desktop/tp_prog/tp/econometrie/TP2/data_angrist_evans.dta')

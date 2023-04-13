4608b96867014be7a9cb5cef4efcca95eb8bfc2d
gabrielderome <gabriel.derome@gmail.com>
2023-04-13 14:53:35 -0400

    print table 2

--- econometrie/TP2/tp2.py
+++ econometrie/TP2/tp2.py
@@ -96,3 +96,143 @@ female = df.loc[df['sex'] == 'Female']
 married_female = df.loc[df['married'] == 1]
 married_male = df.loc[(df['sex'] == 'Male') & (df['married'] == 1)]
 
+#get the number of rows in each df
+female_rows = female.shape[0]
+married_female_rows = married_female.shape[0]
+married_male_rows = married_male.shape[0]
+
+#get the means and std
+mean_nchild_w_all = female['nchild'].mean()
+std_nchild_w_all = female['nchild'].std()
+mean_nchild_w_married = married_female['nchild'].mean()
+std_nchild_w_married = married_female['nchild'].std()
+
+mean_mt2_w_all = female['morethantwo'].mean()
+std_mt2_w_all = female['morethantwo'].std()
+mean_mt2_w_married = married_female['morethantwo'].mean()
+std_mt2_w_married = married_female['morethantwo'].std()
+
+mean_fbm_w_all = female['firstborn_male'].mean()
+std_fbm_w_all = female['firstborn_male'].std()
+mean_fbm_w_married = married_female['firstborn_male'].mean()
+std_fbm_w_married = married_female['firstborn_male'].std()
+
+mean_sbm_w_all = female['secondborn_male'].mean()
+std_sbm_w_all = female['secondborn_male'].std()
+mean_sbm_w_married = married_female['secondborn_male'].mean()
+std_sbm_w_married = married_female['secondborn_male'].std()
+
+mean_tb_w_all = female['twoboys'].mean()
+std_tb_w_all = female['twoboys'].std()
+mean_tb_w_married = married_female['twoboys'].mean()
+std_tb_w_married = married_female['twoboys'].std()
+
+mean_tg_w_all = female['twogirls'].mean()
+std_tg_w_all = female['twogirls'].std()
+mean_tg_w_married = married_female['twogirls'].mean()
+std_tg_w_married = married_female['twogirls'].std()
+
+mean_ss_w_all = female['samesex'].mean()
+std_ss_w_all = female['samesex'].std()
+mean_ss_w_married = married_female['samesex'].mean()
+std_ss_w_married = married_female['samesex'].std()
+
+mean_twins_w_all = female['twins'].mean()
+std_twins_w_all = female['twins'].std()
+mean_twins_w_married = married_female['twins'].mean()
+std_twins_w_married = married_female['twins'].std()
+
+mean_age_w_all = female['age'].mean()
+std_age_w_all = female['age'].std()
+mean_age_w_married = married_female['age'].mean()
+std_age_w_married = married_female['age'].std()
+mean_age_m = married_male['age'].mean() 
+std_age_m = married_male['age'].std()
+
+mean_afb_w_all = female['agefirst'].mean()
+std_afb_w_all = female['agefirst'].std()
+mean_afb_w_married = married_female['agefirst'].mean()
+std_afb_w_married = married_female['agefirst'].std()
+mean_afb_m = married_male['agefirst'].mean()
+std_afb_m = married_male['agefirst'].std()
+
+mean_lf_w_all = female['labforce'].mean()
+std_lf_w_all = female['labforce'].std()
+mean_lf_w_married =  married_female['labforce'].mean()
+std_lf_w_married =  married_female['labforce'].std()
+mean_lf_m = married_male['labforce'].mean()
+std_lf_m = married_male['labforce'].std()
+
+mean_ww_w_all = female['wkswork2'].mean()
+std_ww_w_all = female['wkswork2'].std()
+mean_ww_w_married = married_female['wkswork2'].mean()
+std_ww_w_married = married_female['wkswork2'].std()
+mean_ww_m = married_male['wkswork2'].mean()
+std_ww_m = married_male['wkswork2'].std()
+
+mean_hw_w_all = female['hrswork2'].mean()
+std_hw_w_all = female['hrswork2'].std()
+mean_hw_w_married = married_female['hrswork2'].mean()
+std_hw_w_married = married_female['hrswork2'].std()
+mean_hw_m = married_male['hrswork2'].mean()
+std_hw_m = married_male['hrswork2'].std()
+
+mean_inc_w_all = female['inctot'].mean()
+std_inc_w_all = female['inctot'].std()
+mean_inc_w_married = married_female['inctot'].mean()
+std_inc_w_married = married_female['inctot'].std()
+mean_inc_m = married_male['inctot'].mean()
+std_inc_m = married_male['inctot'].std()
+
+mean_finc_w_all = female['ftotinc'].mean()
+std_finc_w_all = female['ftotinc'].std()
+mean_finc_married = married_female['ftotinc'].mean()
+std_finc_w_married = married_female['ftotinc'].std()
+mean_finc_m = married_male['ftotinc'].mean()
+std_finc_ml = married_male['ftotinc'].std()
+
+mean_nwinc = df['nonwifeinc'].mean()
+std_nwinc = df['nonwifeinc'].std()
+
+#print all means and stds
+print(mean_nchild_w_all, std_nchild_w_all)
+print(mean_nchild_w_married,  std_nchild_w_married)
+print(mean_mt2_w_all, std_mt2_w_all)
+print(mean_mt2_w_all, std_mt2_w_all)
+print(mean_fbm_w_all, std_fbm_w_all)
+print(mean_fbm_w_married, std_fbm_w_married)
+print(mean_sbm_w_all, std_sbm_w_all)
+print(mean_sbm_w_married, std_sbm_w_married)
+print(mean_tb_w_all, std_tb_w_all)
+print(mean_tb_w_married, std_tb_w_married)
+print(mean_tg_w_all, std_tg_w_all)
+print(mean_tg_w_married, std_tg_w_married)
+print(mean_ss_w_all, std_ss_w_all)
+print(mean_ss_w_married, std_ss_w_married)
+print(mean_twins_w_all, std_twins_w_all)
+print(mean_twins_w_married, std_twins_w_married)
+print(mean_age_w_all, std_age_w_all)
+print(mean_age_w_married, std_age_w_married)
+print(mean_age_m, std_age_m)
+print(mean_afb_w_all, std_afb_w_all)
+print(mean_afb_w_married, std_afb_w_married)
+print(mean_afb_m, std_afb_m)
+print(mean_lf_w_all, std_lf_w_all)
+print(mean_lf_w_married, std_lf_w_married)
+print(mean_lf_m, std_lf_m)
+print(mean_ww_w_all, std_ww_w_all)
+print(mean_ww_w_married, std_ww_w_married)
+print(mean_ww_m, std_ww_m)
+print(mean_hw_w_all, std_hw_w_all)
+print(mean_hw_w_married, std_hw_w_married)
+print(mean_hw_m, std_hw_m)
+print(mean_inc_w_all, std_inc_w_all)
+print(mean_inc_w_married, std_inc_w_married)
+print(mean_inc_m, std_inc_m)
+print(mean_finc_w_all, std_finc_w_all)
+print(mean_finc_married, std_finc_w_married)
+print(mean_finc_m, std_finc_ml)
+print(mean_nwinc, std_nwinc)
+print(female_rows, married_female_rows, married_male_rows)
+
+print(df['sex'].value_counts())
